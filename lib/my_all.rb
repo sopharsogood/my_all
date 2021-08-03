a35cd9fa274a7d643c3efc6b065626a52b1f1852
sopharsogood <justanothersophie@gmail.com>
2021-08-03 14:04:29 -0400

    commit tree 5b27458c09e49eefea4e57b2e1a1ce8e4183eb25

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+  index = 0
+  all_return = true
+  while index < collection.length
+    if !!yield(collection[index]) == false
+        all_return = false
+    end
+    index += 1
+  end
+  all_return
 end
+
+# slightly different from the solution the readme suggested, I wanted to do it without relying on #include?
